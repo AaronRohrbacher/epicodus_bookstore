cbcf6d0277691c528aea3083f8cab421d9ad158f
Aaron Rohrbacher and Riki Montgomery <riki@>
2017-12-07 16:52:28 -0800

    clean up front end

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'aaronsnudes@example.com'
+  default from: 'example@example.com'
   layout 'mailer'
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
+
   factory :user do
-    
+    email('aaron@gmail.com')
+    password('password')
+  end
+
+  factory (:admin, class: User) do
+    email('aaron@gmail.com')
+    password('password')
+    admin(true)
   end
 end
